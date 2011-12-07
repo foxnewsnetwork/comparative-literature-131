fa0957599ef30552b3871045e736d86393f42327
Yuki Nagato <foxnewsnetwork@gmail.com>
2011-12-06 16:29:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manga4koma::Application.config.secret_token = '66f220ef3bcaeefa09977ab1e260afe7dd93ff417b96ee761c759465ef1da5553c2514804e1ede70177ec3c2f415b1e9f61412ceba713bc7568bde8c9ef20ab9'
