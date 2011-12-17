9b9b152226ad6aee8553ff1c0a29dea20ccb6fba
Yuki Nagato <foxnewsnetwork@gmail.com>
2011-12-17 12:42:05 -0800

    implemented the twitter-bootstrap to a bunch of pages; more work needs to be done though

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   def show
-  	if user_signed_in?
-  		@is_correct_user = ( current_user.id.to_s == params[:id] )
-  		@picture = Picture.new
-  		@existing_pics = current_user.pictures
-  	else
-  		@is_correct_user = false
-  	end
+  	@user = User.find_by_id(params[:id])
+	@correct_user = ( current_user.id == @user.id ) if user_signed_in?
+	if @correct_user
+		@picture = current_user.pictures.new
+
+	end
   end
 
   def index
